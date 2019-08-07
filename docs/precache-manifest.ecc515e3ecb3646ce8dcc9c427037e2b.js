c2622527079375b753d89cec77cb7703822510cb
Carrie <carrierong37@gmail.com>
2019-08-07 00:49:48 -0400

    Add build

---
+++ docs/precache-manifest.ecc515e3ecb3646ce8dcc9c427037e2b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9302588628c173c231bd18f38b1dd95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aea93de23c4436fd5cc2",
+    "url": "/static/css/main.0db89d8c.chunk.css"
+  },
+  {
+    "revision": "152f019a340eeb9d07a1",
+    "url": "/static/js/2.af2d5a30.chunk.js"
+  },
+  {
+    "revision": "aea93de23c4436fd5cc2",
+    "url": "/static/js/main.a8714b57.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fe9f5eed5532b9a1a95d5e58aeb6a0d0",
+    "url": "/static/media/rocket.fe9f5eed.svg"
+  },
+  {
+    "revision": "8f290421eca0dcf59b5fa9ce5e03906a",
+    "url": "/static/media/sensor1.8f290421.png"
+  },
+  {
+    "revision": "4e008ef02fd47d998ffb9486bbdbe794",
+    "url": "/static/media/sensor2.4e008ef0.png"
+  },
+  {
+    "revision": "61c5d34bb50ea199274c5221cb16b4b4",
+    "url": "/static/media/sensor3.61c5d34b.png"
+  }
+]);
