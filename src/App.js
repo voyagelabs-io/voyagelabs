baac4b2ac98017b7ebc71062e2993ff6bf904374
Carrie <carrierong37@gmail.com>
2019-08-20 13:01:32 -0400

    2nd commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Navbar from './components/Navbar';
 import Main from './components/Main';
 import NotFound from './components/NotFound';
 
+import fb from './img/iconfb.png';
+import twitter from './img/icontwitter.png';
+
+
 const App = () => {
   return (
     <div className="app">
@@ -15,6 +19,11 @@ const App = () => {
           <Route exact path="/" component={Main} />
           <Route component={NotFound} />
         </Switch>
+        <div className="footer">
+          <a href="http://www.facebook.com" target="_blank"><img src={fb} className="icon"/></a>
+          <a href="http://www.twitter.com" target="_blank"><img src={twitter} className="icon"/></a>
+          <a href="http://www.youtube.com" target="_blank"><img src='path_to_your_image' className="icon"/></a>
+         </div>
       </BrowserRouter>
     </div>
   );
