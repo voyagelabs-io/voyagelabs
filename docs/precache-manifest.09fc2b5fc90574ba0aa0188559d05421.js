084ccd1cde04ef7603d4aa7ff396f0abe7e33aa1
Carrie <carrierong37@gmail.com>
2019-08-23 18:12:57 -0400

    docs

---
+++ docs/precache-manifest.09fc2b5fc90574ba0aa0188559d05421.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7a4aa84fcda6b62f49a4a9044e18a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe0076a83cdf33673d89",
+    "url": "/static/css/main.21866586.chunk.css"
+  },
+  {
+    "revision": "152f019a340eeb9d07a1",
+    "url": "/static/js/2.af2d5a30.chunk.js"
+  },
+  {
+    "revision": "fe0076a83cdf33673d89",
+    "url": "/static/js/main.2e11599d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fe9f5eed5532b9a1a95d5e58aeb6a0d0",
+    "url": "/static/media/rocket.fe9f5eed.svg"
+  },
+  {
+    "revision": "cc5471ab5ad5ea96be0291c3323cce7f",
+    "url": "/static/media/sensor1.cc5471ab.png"
+  },
+  {
+    "revision": "b2b95891029a8de3e8303d2e84397036",
+    "url": "/static/media/sensor2.b2b95891.png"
+  },
+  {
+    "revision": "1abc33576a19d5104891069423fbf915",
+    "url": "/static/media/sensor3.1abc3357.png"
+  }
+]);
